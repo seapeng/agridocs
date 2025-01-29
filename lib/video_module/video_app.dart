9a656f82bcfc15d5585e2343799e5b34f30b6f68
Sea Peng <mr.seapeng@gmail.com>
2025-01-29 23:00:41 +0700

    fixed

--- lib/video_module/video_app.dart
+++ lib/video_module/video_app.dart
@@ -10,16 +10,17 @@ class VideoApp extends StatelessWidget {
       home: VideoScreen(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          fontFamily: 'Battambang',
-          appBarTheme: AppBarTheme(
-              backgroundColor: const Color.fromARGB(255, 33, 112, 35),
-              foregroundColor: Colors.white //here you can give the text color
-              ),
-          bottomNavigationBarTheme: BottomNavigationBarThemeData(
-            type: BottomNavigationBarType.fixed,
-            selectedItemColor: Colors.blue,
-            unselectedItemColor: Colors.grey,
-          )),
+        fontFamily: 'Battambang',
+        appBarTheme: AppBarTheme(
+            backgroundColor: const Color.fromARGB(255, 33, 112, 35),
+            foregroundColor: Colors.white //here you can give the text color
+            ),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.grey,
+        ),
+      ),
     );
   }
 }
