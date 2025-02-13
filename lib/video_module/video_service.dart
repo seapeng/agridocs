fdea5960f440c2d1bb1ac270296862d016159c92
Sea Peng <mr.seapeng@gmail.com>
2025-02-13 17:04:55 +0700

    fixed

--- lib/video_module/video_service.dart
+++ lib/video_module/video_service.dart
@@ -5,15 +5,38 @@ import 'video_model.dart';
 class VideoService {
   final String apiUrl = "https://agridocs-api.daovitou.net/mobile/v1/videos";
 
+  // Future<List<VideoModel>> fetchVideos() async {
+  //   final response = await http.get(Uri.parse(apiUrl));
+
+  //   if (response.statusCode == 200) {
+  //     // Parse the JSON response
+  //     List<dynamic> data = json.decode(response.body);
+  //     return data.map((json) => VideoModel.fromJson(json)).toList();
+  //   } else {
+  //     throw Exception('Failed to load videos');
+  //   }
+  // }
+
   Future<List<VideoModel>> fetchVideos() async {
     final response = await http.get(Uri.parse(apiUrl));
 
     if (response.statusCode == 200) {
-      // Parse the JSON response
       List<dynamic> data = json.decode(response.body);
       return data.map((json) => VideoModel.fromJson(json)).toList();
     } else {
       throw Exception('Failed to load videos');
     }
   }
+
+  // Future<List<VideoModel>> fetchVideos() async {
+  //   final response = await http.get(Uri.parse(apiUrl));
+
+  //   if (response.statusCode == 200) {
+  //     Map<String, dynamic> responseBody = json.decode(response.body);
+  //     List<dynamic> data = responseBody['data'];
+  //     return data.map((json) => VideoModel.fromJson(json)).toList();
+  //   } else {
+  //     throw Exception('Failed to load videos');
+  //   }
+  // }
 }
