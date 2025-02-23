b93b552f98460b31141a73c585b35bb7fcd054e9
Sea Peng <mr.seapeng@gmail.com>
2025-02-23 10:53:57 +0700

    add video search layout

--- lib/video_module/video_search_screen.dart
+++ lib/video_module/video_search_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
 
 import 'video_model.dart';
 import 'video_search_logic.dart';
+import 'video_detail.dart';
 
 class VideoSearchScreen extends StatefulWidget {
   const VideoSearchScreen({super.key});
@@ -97,45 +100,157 @@ class _VideoSearchScreenState extends State<VideoSearchScreen> {
         context.read<VideoSearchLogic>().setLoading();
         context.read<VideoSearchLogic>().search(_searchCtrl.text.trim());
       },
-      child: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          mainAxisSpacing: 5,
-          crossAxisSpacing: 5,
-          childAspectRatio: 4 / 6,
-        ),
+      child: ListView.builder(
+        physics: BouncingScrollPhysics(),
         itemCount: items.length,
-        physics: const BouncingScrollPhysics(),
         itemBuilder: (context, index) {
-          return _buildItem(items[index]);
+          return _buildListItem(items[index]);
         },
+      ),
+      // child: GridView.builder(
+      //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      //     crossAxisCount: 1,
+      //     mainAxisSpacing: 5,
+      //     crossAxisSpacing: 5,
+      //     childAspectRatio: 4 / 6,
+      //   ),
+      //   itemCount: items.length,
+      //   physics: const BouncingScrollPhysics(),
+      //   itemBuilder: (context, index) {
+      //     return _buildItem(items[index]);
+      //   },
+      // ),
+    );
+  }
+
+  // Widget _buildItem(Videos item) {
+  //   return Card(
+  //     child: Column(
+  //       children: [
+  //         Expanded(
+  //           child: ClipRRect(
+  //             borderRadius: BorderRadius.circular(10),
+  //             child: Image.network(
+  //               "https://img.youtube.com/vi/${item.youtubeId}/mqdefault.jpg",
+  //               width: double.maxFinite,
+  //               fit: BoxFit.cover,
+  //             ),
+  //           ),
+  //         ),
+  //         Padding(
+  //           padding: const EdgeInsets.all(8.0),
+  //           child: Text(
+  //             item.title,
+  //             maxLines: 1,
+  //             overflow: TextOverflow.ellipsis,
+  //           ),
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
+
+  Widget _buildListItem(Videos video) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Card(
+        child: Column(
+          children: [
+            InkWell(
+              onTap: () => _pageDetail(video),
+              child: SizedBox(
+                width: double.infinity,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                  ),
+                  child: Image.network(
+                    "https://img.youtube.com/vi/${video.youtubeId}/mqdefault.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 10,
+                  bottom: 10,
+                ),
+                child: Text(
+                  video.title,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 20,
+                right: 20,
+                bottom: 10,
+              ),
+              child: Row(
+                children: <Widget>[
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        Icons.today,
+                        size: 12,
+                      ),
+                      SizedBox(width: 3),
+                      Text(
+                        DateFormat('dd-MM-yyyy').format(video.published),
+                        style: TextStyle(fontSize: 12),
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: 20),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        Icons.visibility,
+                        size: 12,
+                      ),
+                      SizedBox(width: 3),
+                      Text(
+                        343.toString(),
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  Widget _buildItem(Videos item) {
-    return Card(
-      child: Column(
-        children: [
-          Expanded(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.network(
-                "https://img.youtube.com/vi/${item.youtubeId}/mqdefault.jpg",
-                width: double.maxFinite,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              item.title,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
-        ],
+  void _pageDetail(Videos video) {
+    Navigator.of(context).push(
+      CupertinoPageRoute(
+        builder: (context) => VideoDetail(
+          id: video.id,
+          title: video.title,
+          youtubeId: video.youtubeId,
+          author: video.author,
+          published: video.published,
+          language: video.language.name,
+          videoCateogy: video.videoCategory.name,
+        ),
       ),
     );
   }
